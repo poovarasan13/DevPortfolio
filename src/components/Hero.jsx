f09d686b2d3de6e223922cd7ee97c8b9b5a52f3a
Poovarasan S <poovarasan936161@gmail.com>
2025-10-01 17:50:45 +0530

    updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import Typewriter from 'typewriter-effect';
 
 const Hero = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const roles = ["MernStack Developer", "Web Designer"];
+  const roles = ["Java Full Stack Developer","MernStack Developer", "Web Designer"];
   const [isLoadingCv, setIsLoadingCv] = useState(false);
   const RESUME_LINK = "https://drive.google.com/file/d/1481n_E8Ohc9v9Aeke3CrX8LJDZ9UhLse/view?usp=sharing";
 
